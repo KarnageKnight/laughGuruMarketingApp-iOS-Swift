78a4334634dc0872c21ceef825e4eaf28aee49a2
KarnageKnight <karanvir95@gmail.com>
2016-04-09 00:39:13 +0530

    stack commit, trying other dropdown solutions

--- SidebarMenu/pricing.swift
+++ SidebarMenu/pricing.swift
@@ -3,15 +3,21 @@
 //  SidebarMenu
 //
 //  Created by KarnageKnight on 07/04/16.
-//  Copyright © 2016 AppCoda. All rights reserved.
+//  Copyright © 2016 KarnageKnight. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
-class pricing:UITableViewController{
+class pricing: UITableViewController{
     @IBOutlet weak var resultView: UIView!
     @IBOutlet weak var filterView: UIView!
+    var dropDownMenu = IGLDropDownMenu()
+    var dataTitle:NSArray = ["CBSE",
+        "ICSE",
+        "SSC",
+        "IGSCE",
+        "IB"]
     @IBAction func backButton(sender: AnyObject) {
     
         self.dismissViewControllerAnimated(true, completion: nil)
@@ -33,5 +39,30 @@ class pricing:UITableViewController{
         filterView.layer.shadowOpacity = 0.8
         
         filterView.layer.shadowRadius = 4
+        setupInit()
+    }
+    
+    func setupInit(){
+        var dropdownItems:NSMutableArray = NSMutableArray()
+        
+        for i in 0...(dataTitle.count-1) {
+            
+            var item = IGLDropDownItem();
+            item.text = "\(dataTitle[i])"
+            dropdownItems.addObject(item)
+        }
+        dropDownMenu.menuText = ""
+        dropDownMenu.dropDownItems = dropdownItems as [AnyObject]
+        dropDownMenu.paddingLeft = 15
+        dropDownMenu.frame = CGRectMake((self.view.frame.size.width/2) - 150, 100, 100, 20)
+        //dropDownMenu.delegate = dropDownMenu()
+        dropDownMenu.type = IGLDropDownMenuType.Stack
+        dropDownMenu.gutterY = 5
+        dropDownMenu.itemAnimationDelay = 0.1
+        //dropDownMenu.rotate = IGLDropDownMenuRotate.Random //add rotate value for tilting the
+        dropDownMenu.reloadView()
+        
+    
+        self.view.addSubview(self.dropDownMenu)
     }
 }
