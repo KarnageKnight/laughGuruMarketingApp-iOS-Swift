f9fb8f759de1f2e97710f8835d7bb13c1ab9d888
KarnageKnight <karanvir95@gmail.com>
2016-04-11 00:46:04 +0530

    Picker Done, weights done; need checkboxes; need final weight

--- SidebarMenu/pricing.swift
+++ SidebarMenu/pricing.swift
@@ -11,12 +11,35 @@ import UIKit
 
 class pricing:UITableViewController,UIPickerViewDataSource, UIPickerViewDelegate{
     @IBOutlet weak var doneButton: UIButton!
- 
+    var boardWeight:Double=0
+    var gradeWeight:Double=0
+    var durationWeight:Double = 0
     @IBAction func pickerDone(sender: AnyObject) {
+        
+        let boardRow:String=myPicker.selectedRowInComponent(0).description
+        let boardChosen = Board[Int(boardRow)!]
+        let gradeRow:String=myPicker.selectedRowInComponent(1).description
+        let gradeChosen = Grade[Int(gradeRow)!]
+        let durationRow:String=myPicker.selectedRowInComponent(2).description
+        let durationChosen = Duration[Int(durationRow)!]
+        
+        let bWeightResult = db.query("select Weight from BoardFactor where Board='\(boardChosen)'", parameters: nil)
+        for row in bWeightResult{
+            boardWeight = row["Weight"] as! Double
+        }
+        let gWeightResult = db.query("select Weight from StandardFactor where Grade='\(gradeChosen)'", parameters: nil)
+        for row in gWeightResult{
+            gradeWeight = row["Weight"] as! Double
+        }
+        let dWeightResult = db.query("select Weight from DurationFactor where Duration='\(durationChosen)'", parameters: nil)
+        for row in dWeightResult{
+            durationWeight = row["Weight"] as! Double
+        }
         myPicker.hidden=true
         doneButton.hidden=true
         
     }
+   let db = SQLiteDB.sharedInstance()
     @IBOutlet weak var myPicker: UIPickerView!
     @IBOutlet weak var resultView: UIView!
     @IBOutlet weak var filterView: UIView!
@@ -24,10 +47,12 @@ class pricing:UITableViewController,UIPickerViewDataSource, UIPickerViewDelegate
     var Grade:[String] = []
     var Duration:[String] = []
     var pickerData:[[String]]=[]
+ //   var boardChosen:String=[]
     @IBAction func backButton(sender: AnyObject) {
     
         self.dismissViewControllerAnimated(true, completion: nil)
     }
+    
     
     override func viewDidLoad() {
         resultView.layer.cornerRadius=9
@@ -48,18 +73,20 @@ class pricing:UITableViewController,UIPickerViewDataSource, UIPickerViewDelegate
         myPicker.dataSource = self
         myPicker.delegate = self
        
-        let db = SQLiteDB.sharedInstance()
+        
         let resultBoard = db.query("select Board from BoardFactor", parameters: nil)
         for row in resultBoard
         {
             Board.append(row["Board"] as! String)
         }
-       let resultGrade = db.query("select CAST(Grade as varchar(10)) from StandardFactor", parameters: nil)
+       let resultGrade = db.query("select Grade from StandardFactor", parameters: nil)
         
         for row in resultGrade
         {
-            print(row["Grade"])
-            Grade.append("\(row["Grade"])")
+            let chara:String = "\(row["Grade"])"
+            var chara1:[String] = chara.componentsSeparatedByString("(")
+            var gradeString:[String]=chara1[1].componentsSeparatedByString(")")
+            Grade.append(gradeString[0])
         }
 
         let resultDuration = db.query("select Duration from DurationFactor", parameters: nil)
